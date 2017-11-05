70903e8880df04cebb1e8bb1e8fbf18bce56af8e
Rimil Dey <rimildeyjsr@gmail.com>
2017-11-05 23:19:55 +0530

    can edit and delete items

--- PullToRefresh/GroceriesTableViewController.swift
+++ PullToRefresh/GroceriesTableViewController.swift
@@ -21,16 +21,13 @@ class GroceriesTableViewController: UITableViewController {
     }
     
     // MARK: - properties
-    
-    var data = ["milk","eggs","lemon","ice cream"]
-    var cloudData = ["milk","eggs","lemon","ice cream","cake","chicken"]
-    
     let pullToRefreshControl = UIRefreshControl()
+    let model = Model()
     
     // MARK: - functions
     
     func refreshTable () {
-        data = cloudData
+        model.data = model.cloudData
         self.tableView.reloadData() 
         pullToRefreshControl.endRefreshing()
     }
@@ -42,53 +39,54 @@ class GroceriesTableViewController: UITableViewController {
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return data.count
+        return model.data.count
     }
 
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "groceriesCell", for: indexPath)
 
-        cell.textLabel?.text = data[indexPath.row]
+        cell.textLabel?.text = model.data[indexPath.row]
 
         return cell
     }
     
 
-    /*
+    
     // Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
         // Return false if you do not want the specified item to be editable.
         return true
     }
-    */
+ 
 
-    /*
+    
     // Override to support editing the table view.
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
+            model.data.remove(at: indexPath.row)
             // Delete the row from the data source
             tableView.deleteRows(at: [indexPath], with: .fade)
         } else if editingStyle == .insert {
             // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
         }    
     }
-    */
+ 
 
-    /*
+    
     // Override to support rearranging the table view.
     override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
 
     }
-    */
+ 
 
-    /*
+    
     // Override to support conditional rearranging of the table view.
     override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
         // Return false if you do not want the item to be re-orderable.
         return true
     }
-    */
+ 
 
     /*
     // MARK: - Navigation
---
+++ PullToRefresh/Model.swift
@@ -0,0 +1,17 @@
+//
+//  Model.swift
+//  PullToRefresh
+//
+//  Created by Rimil Dey on 05/11/17.
+//  Copyright © 2017 Rimil Dey. All rights reserved.
+//
+
+import Foundation
+
+class Model {
+    
+    var data = ["milk","eggs","lemon","ice cream"]
+    var cloudData = ["milk","eggs","lemon","ice cream","cake","chicken"]
+    
+
+}
